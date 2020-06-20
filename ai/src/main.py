27674a9fedbd8f913c26ecdc4997d798c5557839
Tristan Bouillot <tristanblt@gmail.com>
2020-06-20 15:44:59 +0200

    add identificator to AI

--- ai/src/main.py
+++ ai/src/main.py
@@ -14,14 +14,16 @@ from ai.src.game import startGame
 
 def main():
     params = parseArgs()
+    count = 0
 
     while True:
+        count += 1
         pid = os.fork()
         if pid == 0:
             mainsock = initSocket(params)
             if mainsock is None:
                 break
-            startGame(params, mainsock)
+            startGame(params, mainsock, count)
             mainsock.close()
         else:
             time.sleep(5)
